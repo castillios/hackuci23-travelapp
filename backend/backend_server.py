3e56880afcfc38c04784b1da62b56bedd62b1fc3
m-l-kang <melissa@kang-ohana.com>
2023-02-04 11:40:41 -0800

    changed backend_server

--- backend/backend_server.py
+++ backend/backend_server.py
@@ -1,6 +1,6 @@
 from flask import Flask, jsonify
 from flask_cors import CORS
-from distance_matrix import by_interest_and_dist
+from distance_matrix import by_interest_and_dist, extract_attr
 
 app = Flask(__name__)
 CORS(app)
@@ -9,3 +9,5 @@ CORS(app)
 def places_by_interest_and_dist():
 
     return jsonify(by_interest_and_dist(3000, 'restaurants'))
+    #return extract_attr("name", 3000, 'restaurants')
+
