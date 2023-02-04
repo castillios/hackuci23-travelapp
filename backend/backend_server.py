54eb3bb0114ce0c4b42ef353814a1debe06c2401
m-l-kang <melissa@kang-ohana.com>
2023-02-04 12:48:49 -0800

    updated backend_server

--- backend/backend_server.py
+++ backend/backend_server.py
@@ -1,6 +1,6 @@
 from flask import Flask, jsonify
 from flask_cors import CORS
-from distance_matrix import by_interest_and_dist, extract_attr
+from distance_matrix import by_interest_and_dist, get_distance
 
 app = Flask(__name__)
 CORS(app)
@@ -9,5 +9,9 @@ CORS(app)
 def places_by_interest_and_dist():
 
     return jsonify(by_interest_and_dist(3000, 'restaurants'))
-    #return extract_attr("name", 3000, 'restaurants')
+    #names = extract_attr("name", 3000, 'restaurants')
+    #return f"<p>{names}</p>"
 
+@app.route('/byaddr')
+def places_by_dist():
+    return jsonify(get_distance('Costco Wholesale, 2700 Park Ave, Tustin, CA 92782', '30305 Arroyo Dr, Irvine, CA 92617'))
