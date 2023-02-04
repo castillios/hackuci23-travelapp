eb06f0aa6e1a9266065c0a4491668ab54f2247ba
m-l-kang <melissa@kang-ohana.com>
2023-02-04 12:41:37 -0800

    get_distance function works

--- backend/distance_matrix.py
+++ backend/distance_matrix.py
@@ -7,21 +7,28 @@ import json
 # will output json file with results from Google's find place API
 def by_interest_and_dist(distance: int, interest: str):
     #url = "https://maps.googleapis.com/maps/api/distancematrix/json?origins=Washington%2C%20DC&destinations=New%20York%20City%2C%20NY&units=imperial&key=AIzaSyA58JmLlKl8ILlsOtsgKlAO6hYrUCgRDl0"
-    
+    api_key = "AIzaSyA58JmLlKl8ILlsOtsgKlAO6hYrUCgRDl0"
     dist_meters = miles_to_meters(distance)
-    #url = f"https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input={interest}&inputtype=textquery&locationbias=circle%3A{dist_meters}%33.6405%2C117.8443&fields=formatted_address%2Cname%2Crating%2Copening_hours%2Cgeometry&key=AIzaSyA58JmLlKl8ILlsOtsgKlAO6hYrUCgRDl0"
+    
     # https://towardsdatascience.com/how-to-use-the-google-places-api-for-location-analysis-and-more-17e48f8f25b1
-    url = f"https://maps.googleapis.com/maps/api/place/textsearch/json?query={interest}+irvine+ca&radius={dist_meters}&region=us&type=restaurant,gym,university,tourist_attraction,store,shopping_mall,park,museum&key=AIzaSyA58JmLlKl8ILlsOtsgKlAO6hYrUCgRDl0"
+    url = f"https://maps.googleapis.com/maps/api/place/textsearch/json?query={interest}+irvine+ca&radius={dist_meters}&region=us&type=restaurant,gym,university,tourist_attraction,store,shopping_mall,park,museum&key={api_key}"
     payload={}
     headers = {}
 
     response = requests.request("GET", url, headers=headers, data=payload)
-    print(json.dumps(response.json(), indent = 4))
+    #print(json.dumps(response.json(), indent = 4))
     return response.json()
 
-# input origin 
-def by_location(origin:str, end):
-    pass
+# get the time and distance in miles between two locations
+def get_distance(addrFrom: str, addrTo: str):
+    api_key = "AIzaSyA58JmLlKl8ILlsOtsgKlAO6hYrUCgRDl0"
+    formatted_from = addrFrom.replace(' ', '+')
+    formatted_to = addrTo.replace(' ', '+')
+    base_url = "https://maps.googleapis.com/maps/api/distancematrix/json?"
+    full_url = f"{base_url}origins={formatted_from}&destinations={formatted_to}&key={api_key}"
+    response = requests.request("GET", full_url)
+    return response.json()
+
 
 # input number of miles and will output in kilometers to the nearest integer
 def miles_to_meters(miles: int):
@@ -41,5 +48,5 @@ def extract_attr(attr: str, distance: int, interest: str):
 
     return attr_list
 
-
-
+#print(extract_attr("formatted_address", 3000, "restaurants"))
+print(get_distance('Costco Wholesale, 2700 Park Ave, Tustin, CA 92782', '30305 Arroyo Dr, Irvine, CA 92617'))
