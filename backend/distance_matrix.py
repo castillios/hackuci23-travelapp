bd23fa28836501cfadde4a572823bb86a0031cbd
m-l-kang <melissa@kang-ohana.com>
2023-02-04 10:05:52 -0800

    added by interests and places function

--- backend/distance_matrix.py
+++ backend/distance_matrix.py
@@ -1,12 +1,28 @@
 # distance_matrix.py
 
 import requests
+import json
 
-url = "https://maps.googleapis.com/maps/api/distancematrix/json?origins=Washington%2C%20DC&destinations=New%20York%20City%2C%20NY&units=imperial&key=YOUR_API_KEY"
+# input distance in miles and an interest;
+# will output json file with results from Google's find place API
+def by_interest_and_dist(distance: int, interest: str):
+    #url = "https://maps.googleapis.com/maps/api/distancematrix/json?origins=Washington%2C%20DC&destinations=New%20York%20City%2C%20NY&units=imperial&key=AIzaSyA58JmLlKl8ILlsOtsgKlAO6hYrUCgRDl0"
+    
+    dist_meters = miles_to_meters(distance)
+    url = f"https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input={interest}&inputtype=textquery&locationbias=circle%3A{dist_meters}%4047.6918452%2C-122.2226413&fields=formatted_address%2Cname%2Crating%2Copening_hours%2Cgeometry&key=AIzaSyA58JmLlKl8ILlsOtsgKlAO6hYrUCgRDl0"
+    payload={}
+    headers = {}
+    #querystring = {"distance":}
 
-payload={}
-headers = {}
+    response = requests.request("GET", url, headers=headers, data=payload)
+    print(json.dumps(response.json(), indent = 4))
+    return response.json()
 
-response = requests.request("GET", url, headers=headers, data=payload)
+# input number of miles and will output in kilometers to the nearest integer
+def miles_to_meters(miles: int):
+    meters_in_mile = 1609.34
 
-print(response.text)
+    return int(miles*meters_in_mile)
+
+
+by_interest_and_dist(1609, 'restaurants')
